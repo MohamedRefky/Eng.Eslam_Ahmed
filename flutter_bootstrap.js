0bccba2cb58d5effdd4b6cc6e89086bdf38d661f
MohamedRefky <MohamedRefky@users.noreply.github.com>
2026-06-29 20:25:41 +0000

    deploy: 1e0920583948f7b061c65cc2262c1b7438e865ff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2004648732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2424325510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
